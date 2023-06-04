eea6860fc0eeaa1de5e8db199963583ed443b70d
Bogdan Eremin <03bogdan80@gmail.com>
2023-06-04 10:23:36 +0700

    fixes

--- test_app.py
+++ test_app.py
@@ -2,14 +2,14 @@ import unittest
 
 from app import App, UserImage
 
+app = App()
+
 
 class TestApp(unittest.TestCase):
     def test_colors(self):
-        app = App()
         self.test_colors_list = app.user_image.colors
         self.assertEqual(len(self.test_colors_list), 0)
 
     def test_none_image_label(self):
-        app = App()
         self.test_image_label = app.user_image.image_label
         self.assertEqual(app.user_image.image_label.cget("image"), None)
